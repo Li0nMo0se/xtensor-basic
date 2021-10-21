8074f34d6610cc88935ae419f7a2fea86ea2a7c2
Ilan Guenet <ilan.guenet@epita.fr>
2021-10-21 09:47:56 +0200

    up(example): update example with timer between numpy version and c++ version

--- example.py
+++ example.py
@@ -1,6 +1,18 @@
 import mymodule
 import numpy as np
+import timeit
 
-a = np.array([1, 2, 3])
-assert np.isclose(np.sum(np.sin(a)), mymodule.sum_of_sines(a))
+a = np.random.randint(0, 1000, size=10000)
 
+
+def sum_of_sines(a):
+    return mymodule.sum_of_sines(a)
+
+
+assert np.isclose(np.sum(np.sin(a)), sum_of_sines(a))
+
+t = timeit.Timer(lambda: sum_of_sines(a))
+print(t.timeit(100))
+
+t = timeit.Timer(lambda: np.sum(np.sin(a)))
+print(t.timeit(100))
